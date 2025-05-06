201049e8b6eafb29163d802b8ef09923bfde870b
Lydiahm <109420511+Lydiahm@users.noreply.github.com>
2025-05-06 15:54:19 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -29,10 +29,15 @@ def meteo():
         temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c 
         results.append({'Jour': dt_value, 'temp': temp_day_value})
     return jsonify(results=results)
-
+    
 @app.route("/rapport/")
 def mongraphique():
     return render_template("graphique.html")
+
+@app.route('/histogramme/')
+def histogramme():
+    return render_template('histogramme.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
